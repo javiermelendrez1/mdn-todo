58824a321eec8f1b8560e8a9c181760cfe2baf24
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-21 18:28:55 -0700

    new feature added edit
    you can now edit task

--- src/App.js
+++ src/App.js
@@ -27,6 +27,17 @@ function App(props) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   const taskList = tasks.map(task => (
     <Todo
         id={task.id}
@@ -35,6 +46,7 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask={editTask}
       />
     )
   );
